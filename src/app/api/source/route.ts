7bb7e5d93e4b3658071351b32a14e429f501b1d8
daimajia <daimajia@mail.bnu.edu.cn>
2024-07-16 14:52:33 +0800

    support puppeteer source api.

---
+++ src/app/api/source/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export async function GET(request: NextRequest) {
+  const install = require(`puppeteer/internal/node/install.js`).downloadBrowser;
+  await install();
+  const url = request.nextUrl.searchParams.get('url');
+
+  if(!url) return {
+    error: true,
+    source: null
+  }
+
+  const browser = await puppeteer.launch({
+    args: ["--use-gl=angle", "--use-angle=swiftshader", "--single-process", "--no-sandbox"],
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+  const customUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
+  await page.setUserAgent(customUA);
+
+  await page.goto(url);
+  const content = await page.evaluate(() => document.body.innerHTML); 
+  return NextResponse.json({
+    error: false,
+    source: content
+  })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,8 @@
 export const revalidate = 300 // revalidate at most every 5 minutes
-
-import Image from "next/image";
-import Link from "next/link";
-import puppeteer from "puppeteer";
-
 export default async function Home() {
-  const image = await getScreenshot();
-
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Screenshot refreshes every 5 minutes</h1>
-      <Image
-        src={`data:image/png;base64,${image.image.toString("base64")}`}
-        alt={image.title}
-        width={800}
-        height={600}
-      />
-      Source code: <Link href="https://github.com/chromium-for-lambda/chromium-on-vercel">github.com/chromium-for-lambda/chromium-on-vercel</Link>
+      <h1>Puppeteer API</h1>
     </main>
   );
-}
-
-async function getScreenshot() {
-  const install = require(`puppeteer/internal/node/install.js`).downloadBrowser;
-  await install();
-
-  const browser = await puppeteer.launch({
-    args: ["--use-gl=angle", "--use-angle=swiftshader", "--single-process", "--no-sandbox"],
-    headless: true,
-  });
-
-  const page = await browser.newPage();
-
-  const url = `https://news.ycombinator.com`;
-  await page.goto(url);
-
-  const [image, title] = await Promise.all([page.screenshot(), page.title()]);
-
-  await page.close();
-  await browser.close();
-
-  return {
-    title,
-    image,
-  };
 }
